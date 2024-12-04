4191c2ffef34b758f2c1ba345274d4dc9eb9cacb
aejas389 <23hr1a3252@mtieat.org>
2024-12-04 13:30:25 +0530

    Add files via upload

---
+++ aejas2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class aejas2
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+
+        int a=5;
+        int pos=2;
+
+        if((a&pos)==1)
+
+        {
+            System.out.println("Set Bit"); 
+        }
+        else{
+            System.out.println("Not a Set Bit");
+        }
+    }
+           
+}
+    
