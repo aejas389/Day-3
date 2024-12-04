0ce4e207876b3944c7502b41ae980f1306f5e166
aejas389 <23hr1a3252@mtieat.org>
2024-12-04 14:16:36 +0530

    Add files via upload

---
+++ aejas13.java
@@ -0,0 +1,22 @@
+public class aejas13
+ {
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=2;
+        int c=8;
+
+        if((a>b && a>c))
+        {
+            System.out.println("a is bigger");
+        }
+        else if((b>a) && (b>c))
+        {
+            System.out.println("b is bigger");
+        }
+        else
+        {
+            System.out.println("c is bigger");
+        }
+    }
+}
