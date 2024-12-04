051eeb31c1b4a2f0def7119f385ceb8d132064f5
aejas389 <23hr1a3252@mtieat.org>
2024-12-04 13:32:10 +0530

    Add files via upload

---
+++ aejas6.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class aejas6 {
+    public static void main(String[] args) {
+        int a=8388608;
+        if((a&(a-1))==0){
+            System.out.println("POwer of 2");
+        }else{
+            System.out.println("Not a power of 2");
+        }    
+        
+    }
+}
+
+
+
+    
+
