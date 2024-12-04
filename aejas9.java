5c10b74f46a62f8767617044aa2e6c16835aaf8c
aejas389 <23hr1a3252@mtieat.org>
2024-12-04 13:34:20 +0530

    Add files via upload

---
+++ aejas9.java
@@ -0,0 +1,15 @@
+
+public class aejas9{
+    public static void main(String[] args) {
+        int a=5;
+        if((a&1)==0)
+        {
+            System.out.println("Even");
+
+        }
+        else
+        {
+            System.out.println("Odd");
+        }
+    }
+}
