500b2d31628f49307fa5fd495b89efc152235d88
aejas389 <23hr1a3252@mtieat.org>
2024-12-04 13:32:29 +0530

    Add files via upload

---
+++ aejas7.java
@@ -0,0 +1,17 @@
+public class aejas7
+ {
+    public static void main(String[] args)
+     {
+        int a=5;
+        if((a&(a-1))==0)
+        {
+            System.out.println("is a power of 2");
+        }
+        else
+        {
+            System.out.println("is not power of 2");
+        }
+        
+    } 
+    
+}
